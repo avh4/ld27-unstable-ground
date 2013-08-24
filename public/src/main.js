470f84b9f0f98fe050a159ae43736d8b6a2d0ce4
Aaron VonderHaar <gruen0aermel@gmail.com>
2013-08-23 22:22:35 -0700

    Draw player

--- public/src/main.js
+++ public/src/main.js
@@ -15,15 +15,22 @@ function(domReady) {
 			var image = queue.getResult("eyelid_top");
 
 			var stage = new createjs.Stage("canvas");
-
+			
 			var background = new createjs.Bitmap(queue.getResult("demolition"));
 			stage.addChild(background);
+
+			var player = new createjs.Shape();
+			player.graphics.beginFill("#ff0000").drawRect(-34/2, -60, 34, 60);
+			player.graphics.beginFill("#000000").drawCircle(-1, -1, 3, 3);
+			player.y = 500;
+			player.x = 20;
+			stage.addChild(player);
 
 			eyelid = new createjs.Bitmap(image);
 			eyelid.y = -631;
 			stage.addChild(eyelid);
 
-			createjs.Tween.get(eyelid).to({y:0}, 1000);
+			// createjs.Tween.get(eyelid).to({y:0}, 1000);
 
 			createjs.Ticker.setFPS(60);
 			createjs.Ticker.addListener(stage);
