99993ff756c90689a3736f3e34cc21d64473b822
Aaron VonderHaar <gruen0aermel@gmail.com>
2013-08-23 22:17:39 -0700

    Show eyes closing.

--- public/src/main.js
+++ public/src/main.js
@@ -1,6 +1,32 @@
 require(["domReady"],
 function(domReady) {
 	domReady(function() {
-		alert("x");
+
+		var queue = new createjs.LoadQueue();
+		queue.installPlugin(createjs.Sound);
+		queue.addEventListener("complete", handleComplete);
+		queue.loadManifest([
+			{id: "eyelid_top", src:"img/eyelid_top.png"},
+			{id: "demolition", src:"img/demolition.png"}
+		]);
+
+		function handleComplete() {
+			createjs.Sound.play("sound");
+			var image = queue.getResult("eyelid_top");
+
+			var stage = new createjs.Stage("canvas");
+
+			var background = new createjs.Bitmap(queue.getResult("demolition"));
+			stage.addChild(background);
+
+			eyelid = new createjs.Bitmap(image);
+			eyelid.y = -631;
+			stage.addChild(eyelid);
+
+			createjs.Tween.get(eyelid).to({y:0}, 1000);
+
+			createjs.Ticker.setFPS(60);
+			createjs.Ticker.addListener(stage);
+		}
 	});
 });
