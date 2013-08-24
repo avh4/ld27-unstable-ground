a88c877662522939c51f90178ebb5986a9fcb935
Aaron VonderHaar <gruen0aermel@gmail.com>
2013-08-23 23:32:29 -0700

    Add building.

--- public/src/main.js
+++ public/src/main.js
@@ -7,30 +7,41 @@ function(domReady) {
 		queue.addEventListener("complete", handleComplete);
 		queue.loadManifest([
 			{id: "eyelid_top", src:"img/eyelid_top.png"},
-			{id: "demolition", src:"img/demolition.png"}
+			{id: "demolition", src:"img/demolition.png"},
+			{id: "b1", src:"img/b1.png"},
+			{id: "b1i", src:"img/b1i.png"}
 		]);
-
+		
 		function handleComplete() {
-			var image = queue.getResult("eyelid_top");
-
 			var stage = new createjs.Stage("canvas");
 			
 			var background = new createjs.Bitmap(queue.getResult("demolition"));
 			stage.addChild(background);
 
+			b1i = new createjs.Bitmap(queue.getResult("b1i"));
+			b1i.x = 418; b1i.y = 200;
+			stage.addChild(b1i);
+			b1 = new createjs.Bitmap(queue.getResult("b1"));
+			b1.x = 418 + 1; b1.y = 200 + 2;
+			stage.addChild(b1);
+
 			var player = new createjs.Shape();
 			player.graphics.beginFill("#ff0000").drawRect(-34/2, -60, 34, 60);
 			player.graphics.beginFill("#000000").drawCircle(-1, -1, 3, 3);
 			player.y = 500;
-			player.x = 20;
+			player.x = 400;
 			stage.addChild(player);
 
-			eyelid = new createjs.Bitmap(image);
+			eyelid = new createjs.Bitmap(queue.getResult("eyelid_top"));
 			eyelid.y = -631;
 			stage.addChild(eyelid);
-
+			
 			createjs.Ticker.setFPS(60);
 			createjs.Ticker.addListener(stage);
+			
+			function updateBuilding() {
+				b1.visible = player.y >= 500;
+			}
 			
 			keypress.combo("d", function() {
 			    createjs.Tween.get(eyelid).to({y:0}, 1000);
@@ -43,6 +54,14 @@ function(domReady) {
 			});
 			keypress.combo("left", function() {
 				player.x -= 10;
+			});
+			keypress.combo("up", function() {
+				player.y -= 30;
+				updateBuilding();
+			});
+			keypress.combo("down", function() {
+				player.y += 30;
+				updateBuilding();
 			});
 		}
 	});
